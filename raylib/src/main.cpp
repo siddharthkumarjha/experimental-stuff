a668abc1d26881ec5ae23f7e3e8089a65b54145b
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2024-12-11 22:43:00 +0530

    Feat(raylib): made a bouncing rectangle

--- raylib/src/main.cpp
+++ raylib/src/main.cpp
@@ -6,23 +6,23 @@ int main(void)
     InitWindow(500, 500, "My first window");
 
     int x = 0, y = 0;
-    int dx = 1, dy = 0;
+    int dx = 1, dy = 1;
 
     using namespace std::chrono_literals;
     while(!WindowShouldClose())
     {
-        // if(x <= 0 or x >= (GetScreenWidth() - 50))
-        //     dx = -dx;
-        // if(y <= 0 or y >= (GetScreenHeight() - 50))
-        //     dy = -dy;
-
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
         DrawRectangle(x, y, 50, 50, RED);
         EndDrawing();
 
         x += dx;
         y += dy;
+
+        if(x == 0 or x == (GetScreenWidth() - 50))
+            dx = -dx;
+        if(y == 0 or y == (GetScreenHeight() - 50))
+            dy = -dy;
         std::this_thread::sleep_for(10ms);
     }
     CloseWindow();
