7ef370b69d37efbe91ac8246aaf8aed2a0fdec22
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2024-12-15 14:48:04 +0530

    Feat(regex): rust test crate re-org

--- regex/test/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! This crate speicifally exists to test all other crates
-//!
-//! currently the below crate is handled
-//!
-//! - [regex] crate
-//!
-//! any other crates added in future will have their tests added here as well
-
-pub mod regex_ha_test;
