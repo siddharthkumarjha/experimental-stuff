e082bb00c0a8dc7105789ad6e2acb9ccf61ce263
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2025-05-11 21:26:19 +0530

    using a more terse example for unique_ptr

--- unique_ptr.c
+++ unique_ptr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 typedef void *pointer;
@@ -8,6 +9,7 @@ typedef void (*dealloc_fn_t)(pointer);
 typedef struct unique_ptr_t
 {
     pointer ptr;
+    size_t size_of;
     dealloc_fn_t dealloc_fn;
 } unique_ptr_t;
 
@@ -17,22 +19,34 @@ static void cleanup_heap_pointer(unique_ptr_t *ptr)
         ptr->dealloc_fn(ptr->ptr);
 }
 
+#define SIZEOF(P) P.size_of
+#define LEN(TYPE, P) (SIZEOF(P) / sizeof(TYPE))
+
 #define GET_TYPE_CAST_MACRO(_1, _2, NAME, ...) NAME
-#define GET_WITH_TYPE_CAST(type, P) ((type *)(P.ptr))
+#define GET_WITH_TYPE_CAST(TYPE, P) ((TYPE *)(P.ptr))
 #define GET_WITH_NO_CAST(P) (P.ptr)
 #define GET(...)                                                               \
     GET_TYPE_CAST_MACRO(__VA_ARGS__, GET_WITH_TYPE_CAST,                       \
                         GET_WITH_NO_CAST)(__VA_ARGS__)
 
 #define NULL_UNIQUE_PTR                                                        \
-    (unique_ptr_t) { .ptr = NULL, .dealloc_fn = NULL }
+    (unique_ptr_t) { .ptr = NULL, .size_of = 0, .dealloc_fn = NULL }
+
+#define IS_NULL(P) (GET(P) == NULL)
+
+#define ALLOC_SZ(TYPE, SIZE) sizeof(TYPE[SIZE])
+#define ALLOC(TYPE, SIZE) ((SIZE) > 0 ? malloc(ALLOC_SZ(TYPE, SIZE)) : NULL)
 
 #define GET_MK_UNQ_MACRO(_1, _2, _3, NAME, ...) NAME
-#define ALLOC(type, size) ((size) > 0 ? malloc(sizeof(type[size])) : NULL)
-#define MAKE_UNIQUE_WITH_CUSTOM_DELETER(type, size, deleter)                   \
-    (unique_ptr_t) { .ptr = ALLOC(type, size), .dealloc_fn = deleter }
-#define MAKE_UNIQUE_WITH_DEFAULT_DELETER(type, size)                           \
-    (unique_ptr_t) { .ptr = ALLOC(type, size), .dealloc_fn = free }
+#define MAKE_UNIQUE_WITH_CUSTOM_DELETER(TYPE, SIZE, DELETER)                   \
+    (unique_ptr_t)                                                             \
+    {                                                                          \
+        .ptr = ALLOC(TYPE, SIZE), .size_of = ALLOC_SZ(TYPE, SIZE),             \
+        .dealloc_fn = DELETER                                                  \
+    }
+#define MAKE_UNIQUE_WITH_DEFAULT_DELETER(TYPE, SIZE)                           \
+    MAKE_UNIQUE_WITH_CUSTOM_DELETER(TYPE, SIZE, free)
+
 #define MAKE_UNIQUE(...)                                                       \
     GET_MK_UNQ_MACRO(__VA_ARGS__, MAKE_UNIQUE_WITH_CUSTOM_DELETER,             \
                      MAKE_UNIQUE_WITH_DEFAULT_DELETER)(__VA_ARGS__)
@@ -56,23 +70,22 @@ static void print_ptr_arr(unique_ptr_t *a)
     DECLARE_UNIQUE_PTR(tmp_unq_ptr) = NULL_UNIQUE_PTR;
     TAKE_PTR_OWNERSHIP(tmp_unq_ptr, *a);
 
-    for (size_t i = 0; i < SIZE_OF_ARR; ++i)
-        printf("%d ", GET(int, tmp_unq_ptr)[i]);
-    printf("\n");
+    write(STDOUT_FILENO, GET(tmp_unq_ptr), SIZEOF(tmp_unq_ptr));
+    putchar('\n');
 }
 
 int main(void)
 {
-    DECLARE_UNIQUE_PTR(a) = MAKE_UNIQUE(int, SIZE_OF_ARR);
-    if (!GET(a))
+    DECLARE_UNIQUE_PTR(a) = MAKE_UNIQUE(char, SIZE_OF_ARR);
+    if (IS_NULL(a))
     {
         const char err_msg[]     = "out of mem";
         const size_t err_msg_len = (sizeof err_msg) / (sizeof err_msg[0]);
         write(STDERR_FILENO, err_msg, err_msg_len);
+        return EXIT_FAILURE;
     }
 
-    for (size_t i = 0; i < SIZE_OF_ARR; ++i)
-        GET(int, a)[i] = i;
+    memcpy(GET(a), "Hello, World!", sizeof("Hello, World!"));
 
     print_ptr_arr(&a);
     return EXIT_SUCCESS;
